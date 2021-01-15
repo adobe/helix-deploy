26c615458325fe9dea6d75dda8cb2efd570af5ae
Tobias Bocanegra <tripod@adobe.com>
2021-01-15 13:50:28 +0900

    chore: adding scripts for deleting routes and integrations [skip ci]

---
+++ src/scripts/delete-ci-integrations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+aws --profile adobe apigatewayv2 get-integrations --api-id lqmig3v5eb > ints.json
+echo "$(cat ints.json | jq '.Items | length') integrations. deleting..."
+
+cat ints.json \
+  | jq  '.Items[] | select(.IntegrationUri|test(":ci")) | .IntegrationId' \
+  | xargs -I@ aws --region us-east-1 --profile adobe apigatewayv2 delete-integration --api-id lqmig3v5eb --integration-id @
+
+aws apigatewayv2 get-routes --api-id lqmig3v5eb --profile adobe --region us-east-1 > routes.json
+aws --profile adobe apigatewayv2 get-integrations --api-id lqmig3v5eb > ints.json
+echo "$(cat ints.json | jq '.Items | length') integrations remaining."
