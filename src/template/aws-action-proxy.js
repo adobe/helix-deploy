897aa239da7fef30cac54aa9f4c0ad32b714c532
Tobias Bocanegra <tripod@adobe.com>
2021-01-26 18:37:23 +0900

    feat(aws): add option to deploy function proxy
    fixes #45

---
+++ src/template/aws-action-proxy.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-disable no-console */
+// eslint-disable-next-line import/no-unresolved
+const AWS = require('aws-sdk');
+const { promisify } = require('util');
+
+/**
+ * Proxy function that invokes the respective action based on the route parameters.
+ *
+ * The following routes need to use this function:
+ *
+ * ANY /helix-services/{action}/{version}
+ * ANY /helix-services/{action}/{version}/{path+}
+ *
+ * Note that the package (helix-services) is currently hardcoded for simplicity.
+ *
+ * TODO: test with package in route.
+ *
+ * @param event
+ * @returns {Promise<{body, statusCode}|any>}
+ */
+exports.handler = async (event) => {
+  const lambda = new AWS.Lambda();
+  const invoke = promisify(lambda.invoke.bind(lambda));
+
+  const { action, version } = event.pathParameters;
+  const FunctionName = `helix-services--${action}:${version.replace(/\./g, '_')}`;
+
+  try {
+    console.log(`invoking: ${FunctionName}`);
+    const ret = await invoke({
+      FunctionName,
+      Payload: JSON.stringify(event),
+    });
+    const data = JSON.parse(ret.Payload);
+    console.log(`statusCode: ${data.statusCode}`);
+    return data;
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: err.statusCode,
+      body: err.message,
+    };
+  }
+};
