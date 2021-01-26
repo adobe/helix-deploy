f8b6e93d9f8b1e0c034c8ba16dff1ae27efc1ea2
Tobias Bocanegra <tripod@adobe.com>
2021-01-26 10:17:45 +0900

    chore: add pages ci to delete routes [skip ci]

--- src/scripts/delete-ci-routes.sh
+++ src/scripts/delete-ci-routes.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 aws apigatewayv2 get-routes --api-id lqmig3v5eb --profile adobe --region us-east-1 > routes.json
 echo "$(cat routes.json | jq '.Items | length') routes. deleting..."
 cat routes.json \
-  | jq  '.Items[] | select(.RouteKey|test("/ci")) | .RouteId' \
+  | jq  '.Items[] | select(.RouteKey|test("/ci|pages_ci")) | .RouteId' \
   | xargs -I@ aws --region us-east-1 --profile adobe apigatewayv2 delete-route --api-id lqmig3v5eb --route-id @
 
 aws apigatewayv2 get-routes --api-id lqmig3v5eb --profile adobe --region us-east-1 > routes.json
