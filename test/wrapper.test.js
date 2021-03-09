5dfc718b5dfedbe57f05779d0d69fad39f2975cf
Tobias Bocanegra <tripod@adobe.com>
2021-03-09 15:26:42 +0900

    fix(adapter): handle illegal request header errors with 400
    fix #154

--- test/wrapper.test.js
+++ test/wrapper.test.js
@@ -45,6 +45,34 @@ describe('Wrapper tests for Azure', () => {
     await azure(context, request);
     assert.equal(context.res.status, 200, context.res.body);
   });
+
+  it('handles illegal request headers with 400', async () => {
+    const azure = proxyquire('../src/template/index.js', {
+      './main.js': {
+        main: () => new Response('ok'),
+      },
+      './params.json': {
+        FOO: 'bar',
+      },
+    });
+
+    const context = {
+      // eslint-disable-next-line no-console
+      log: console.log,
+      executionContext: {
+        functionName: 'simple-package--simple-name_1_45_0',
+      },
+    };
+    const request = {
+      url: 'https://deploy-helix.azurewebsites.net/api/simple-package/simple-name/1.45.0/foo',
+      headers: {
+        accept: 'жsome value',
+      },
+    };
+
+    await azure(context, request);
+    assert.equal(context.res.status, 400, context.res.body);
+  });
 });
 
 describe('Wrapper tests for OpenWhisk', () => {
@@ -93,9 +121,73 @@ describe('Wrapper tests for OpenWhisk', () => {
     const result = await main(params);
     assert.equal(result.statusCode, 200);
   });
+
+  it('handles illegal request headers with 400', async () => {
+    const { main } = proxyquire('../src/template/index.js', {
+      './main.js': {
+        main: () => new Response('ok'),
+      },
+    });
+    const params = {
+      __ow_method: 'get',
+      __ow_headers: {
+        accept: 'жsome value',
+      },
+    };
+    const result = await main(params);
+    assert.equal(result.statusCode, 400);
+  });
 });
 
 describe('Wrapper tests for AWS', () => {
+  const DEFAULT_EVENT = {
+    version: '2.0',
+    routeKey: 'ANY /dump',
+    rawPath: '/dump',
+    rawQueryString: '',
+    headers: {
+      accept: '*/*',
+      'content-length': '0',
+      host: 'kvvyh7ikcb.execute-api.us-east-1.amazonaws.com',
+      'user-agent': 'curl/7.64.1',
+      'x-amzn-trace-id': 'Root=1-603df0bb-05e846307a6221f72030fe68',
+      'x-forwarded-for': '210.153.232.90',
+      'x-forwarded-port': '443',
+      'x-forwarded-proto': 'https',
+    },
+    requestContext: {
+      accountId: '118435662149',
+      apiId: 'kvvyh7ikcb',
+      domainName: 'kvvyh7ikcb.execute-api.us-east-1.amazonaws.com',
+      domainPrefix: 'kvvyh7ikcb',
+      http: {
+        method: 'GET',
+        path: '/dump',
+        protocol: 'HTTP/1.1',
+        sourceIp: '210.153.232.90',
+        userAgent: 'curl/7.64.1',
+      },
+      requestId: 'bjKNYhHcoAMEJIw=',
+      routeKey: 'ANY /dump',
+      stage: '$default',
+      time: '02/Mar/2021:08:00:59 +0000',
+      timeEpoch: 1614672059918,
+    },
+    isBase64Encoded: false,
+  };
+
+  const DEFAULT_CONTEXT = {
+    getRemainingTimeInMillis: () => 30000,
+    callbackWaitsForEmptyEventLoop: true,
+    functionVersion: '$LATEST',
+    functionName: 'dump',
+    memoryLimitInMB: '128',
+    logGroupName: '/aws/lambda/dump',
+    logStreamName: '2021/03/02/[$LATEST]89b58159f93949f787eb8de043937bbb',
+    invokedFunctionArn: 'arn:aws:lambda:us-east-1:118435662149:function:dump:4_3_1',
+    awsRequestId: '535f0399-9c90-4042-880e-620cfec6af55',
+  };
+
   it('context.func', async () => {
     const { lambda } = proxyquire('../src/template/index.js', {
       './main.js': {
@@ -110,56 +202,51 @@ describe('Wrapper tests for AWS', () => {
       },
       './aws-package-params.js': () => ({}),
     });
-
-    const event = {
-      version: '2.0',
-      routeKey: 'ANY /dump',
-      rawPath: '/dump',
-      rawQueryString: '',
-      headers: {
-        accept: '*/*',
-        'content-length': '0',
-        host: 'kvvyh7ikcb.execute-api.us-east-1.amazonaws.com',
-        'user-agent': 'curl/7.64.1',
-        'x-amzn-trace-id': 'Root=1-603df0bb-05e846307a6221f72030fe68',
-        'x-forwarded-for': '210.153.232.90',
-        'x-forwarded-port': '443',
-        'x-forwarded-proto': 'https',
-      },
-      requestContext: {
-        accountId: '118435662149',
-        apiId: 'kvvyh7ikcb',
-        domainName: 'kvvyh7ikcb.execute-api.us-east-1.amazonaws.com',
-        domainPrefix: 'kvvyh7ikcb',
-        http: {
-          method: 'GET',
-          path: '/dump',
-          protocol: 'HTTP/1.1',
-          sourceIp: '210.153.232.90',
-          userAgent: 'curl/7.64.1',
-        },
-        requestId: 'bjKNYhHcoAMEJIw=',
-        routeKey: 'ANY /dump',
-        stage: '$default',
-        time: '02/Mar/2021:08:00:59 +0000',
-        timeEpoch: 1614672059918,
-      },
-      isBase64Encoded: false,
-    };
-
-    const context = {
-      getRemainingTimeInMillis: () => 30000,
-      callbackWaitsForEmptyEventLoop: true,
-      functionVersion: '$LATEST',
-      functionName: 'dump',
-      memoryLimitInMB: '128',
-      logGroupName: '/aws/lambda/dump',
-      logStreamName: '2021/03/02/[$LATEST]89b58159f93949f787eb8de043937bbb',
-      invokedFunctionArn: 'arn:aws:lambda:us-east-1:118435662149:function:dump:4_3_1',
-      awsRequestId: '535f0399-9c90-4042-880e-620cfec6af55',
-    };
-
-    const res = await lambda(event, context);
+    const res = await lambda(DEFAULT_EVENT, DEFAULT_CONTEXT);
     assert.equal(res.statusCode, 200);
+  });
+
+  it('handles illegal request headers with 400', async () => {
+    const { lambda } = proxyquire('../src/template/index.js', {
+      './main.js': {
+        main: () => new Response('ok'),
+      },
+      './aws-package-params.js': () => ({}),
+    });
+    const res = await lambda({
+      ...DEFAULT_EVENT,
+      headers: {
+        host: 'kvvyh7ikcb.execute-api.us-east-1.amazonaws.com',
+        accept: 'жsome value',
+      },
+    }, DEFAULT_CONTEXT);
+    assert.equal(res.statusCode, 400);
+  });
+});
+
+describe('Wrapper tests for Google', () => {
+  it('handles illegal request headers with 400', async () => {
+    const { google } = proxyquire('../src/template/index.js', {
+      './main.js': {
+        main: () => new Response('ok'),
+      },
+    });
+    const req = {
+      url: 'https://deploy-helix.azurewebsites.net/api/simple-package/simple-name/1.45.0/foo',
+      headers: {
+        accept: 'жsome value',
+      },
+    };
+    const res = {
+      code: 999,
+      status(value) {
+        this.code = value;
+        return this;
+      },
+      send() {
+      },
+    };
+    await google(req, res);
+    assert.equal(res.code, 400);
   });
 });
