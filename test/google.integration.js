70293539b906ef020ef3d3fb3a8ccd8046004699
Lars Trieloff <trieloff@adobe.com>
2021-03-16 16:44:25 +0100

    test(google): expect correct output

--- test/google.integration.js
+++ test/google.integration.js
@@ -87,6 +87,6 @@ describe('Google Integration Test', () => {
     const res = await builder.run();
     assert.ok(res);
     const out = builder.cfg._logger.output;
-    assert.ok(out.indexOf('{"url":"https://us-central1-helix-225321.cloudfunctions.net/simple-package--simple-status_1_45_0/foo","file":"Hello, world.\\n"}') > 0, out);
+    assert.ok(out.indexOf('{"url":"https://us-central1-helix-225321.cloudfunctions.net/simple-package--simple-status_1_45_0/foo"}') > 0, out);
   }).timeout(5000000);
 });
