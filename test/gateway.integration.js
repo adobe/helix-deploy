b340c978dcb3bd81e53d37cc6a801a713e176dc6
Tobias Bocanegra <tripod@adobe.com>
2021-09-24 17:49:19 +0200

    chore: fix tests

--- test/gateway.integration.js
+++ test/gateway.integration.js
@@ -82,7 +82,7 @@ describe('Gateway Integration Test', () => {
     const out = builder.cfg._logger.output;
     const { namespace } = builder._deployers.wsk._cfg;
     assert.ok(out.indexOf(`ok: 200
-{"url":"https://adobeioruntime.net/api/v1/web/${namespace}/simple-package/simple-name@1.45.0/foo","file":"Hello, world.\\n"}`) > 0, out);
+{"url":"https://adobeioruntime.net/api/v1/web/${namespace}/simple-package/simple-name@1.45.0/foo?testPackageParam=42&test-package-param=42","file":"Hello, world.\\n"}`) > 0, out);
 
     const { fetch } = fetchContext();
     const respRandom = await fetch('https://deploy-test.anywhere.run/simple-package/simple-name@1.45.0/foo');
