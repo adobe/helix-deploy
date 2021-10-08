48c243e1ef4bb6819e1ffb6ac8f3c9eab2ce8c5b
Lars Trieloff <trieloff@adobe.com>
2021-10-08 15:42:34 +0200

    test(cloudflare): allow edge-action to make backend requests

--- test/fixtures/edge-action/src/index.js
+++ test/fixtures/edge-action/src/index.js
@@ -9,8 +9,9 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const { Response } = require('@adobe/helix-fetch');
+const { Response, fetch } = require('@adobe/helix-fetch');
 
 module.exports.main = async function main(req, context) {
-  return new Response(`ok: ${await context.env.HEY} ${await context.env.FOO} – ${req.url}`);
+  const backendresponse = await fetch(`https://httpbin.org/status/${req.url.split('/').pop()}`);
+  return new Response(`ok: ${await context.env.HEY} ${await context.env.FOO} – ${backendresponse.status}`);
 };
