35c8f545a027661d7beb5d6ce87f0d1246d25fe6
Maximilian Voß <maximilian.voss@gmail.com>
2023-12-13 23:55:59 +0100

    fix: creating consistency for context.suffix over all adapters
    * Creating consistency for context.suffix over all adapters - others only contain the request path, not the fully qualified URL.

--- src/template/fastly-adapter.js
+++ src/template/fastly-adapter.js
@@ -51,7 +51,7 @@ async function handler(event) {
     const context = {
       resolver: null,
       pathInfo: {
-        suffix: request.url.replace(/\?.*/, ''),
+        suffix: request.url.match(/^https?:\/\/[^/]+([^?]+)/)[1],
       },
       runtime: {
         name: 'compute-at-edge',
