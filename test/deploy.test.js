1cd0db8a140567a381303a30c537fcd113e7dc1d
Max Edell <maxed@adobe.com>
2024-06-07 14:59:14 -0700

    fix: restore cf worker settings after deploy

--- test/deploy.test.js
+++ test/deploy.test.js
@@ -284,6 +284,8 @@ describe('Deploy Test', () => {
 
     let body;
     nock('https://api.cloudflare.com')
+      .get('/client/v4/accounts/123/workers/scripts/default--test-worker/script-settings')
+      .reply(404)
       .post('/client/v4/accounts/123/storage/kv/namespaces', (b) => {
         body = b;
         return true;
@@ -312,6 +314,77 @@ describe('Deploy Test', () => {
     const res = await builder.run();
 
     assert.deepEqual(body, { title: 'default--secrets' });
+
+    assert.deepEqual(res, {
+      cloudflare: {
+        name: 'cloudflare;host=https://null',
+        url: 'default--test-worker',
+      },
+    });
+  }).timeout(15000);
+
+  it('deploys a cloudflare worker and restores existing script settings', async () => {
+    await fse.copy(path.resolve(__rootdir, 'test', 'fixtures', 'cf-worker'), testRoot);
+
+    const bodies = { namespaces: undefined, settings: undefined };
+    nock('https://api.cloudflare.com')
+      .get('/client/v4/accounts/123/workers/scripts/default--test-worker/script-settings')
+      .reply(200, JSON.stringify({
+        success: true,
+        result: {
+          logpush: true,
+          tail_consumers: [
+            {
+              environment: 'production',
+              namespace: 'my-namespace',
+              service: 'my-log-consumer',
+            },
+          ],
+        },
+      }))
+      .post('/client/v4/accounts/123/storage/kv/namespaces', (b) => {
+        bodies.namespaces = b;
+        return true;
+      })
+      .reply(200, JSON.stringify({ result: { id: 'test-namespace' } }))
+      .put('/client/v4/accounts/123/workers/scripts/default--test-worker')
+      .reply(200)
+      .patch('/client/v4/accounts/123/workers/scripts/default--test-worker/script-settings', (b) => {
+        bodies.settings = b;
+        return true;
+      })
+      .reply(200);
+
+    process.chdir(testRoot); // need to change .cwd() for yargs to pickup `wsk` in package.json
+    const builder = new CLI()
+      .prepare([
+        '--build',
+        '--target', 'cloudflare',
+        '--arch', 'edge',
+        '--verbose',
+        '--deploy',
+        '--entryFile', 'index.js',
+        '--directory', testRoot,
+        '--cloudflare-email', 'fake@email.test',
+        '--cloudflare-account-id', '123',
+        '--cloudflare-auth', 'test-token',
+        '--name', 'test-worker',
+      ]);
+    builder.cfg._logger = new TestLogger();
+
+    const res = await builder.run();
+
+    assert.deepEqual(bodies.namespaces, { title: 'default--secrets' });
+    assert.deepEqual(bodies.settings, {
+      logpush: true,
+      tail_consumers: [
+        {
+          environment: 'production',
+          namespace: 'my-namespace',
+          service: 'my-log-consumer',
+        },
+      ],
+    });
 
     assert.deepEqual(res, {
       cloudflare: {
