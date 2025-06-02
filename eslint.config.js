aca8848b7860c04ec5ce130d4e2bc2847d763e03
Dominique Pfister <dpfister@adobe.com>
2025-06-02 17:38:10 +0200

    chore: use new eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import importPlugin from 'eslint-plugin-import';
+import { defineConfig, globalIgnores } from '@eslint/config-helpers'
+import { recommended, source, test } from '@adobe/eslint-config-helix';
+
+export default defineConfig([
+  globalIgnores([
+    'dist',
+    'test/tmp',
+    'src/index.js',
+    'node_modules',
+  ]),
+  {
+    rules: {
+      'import/extensions': ['error', 'ignorePackages'],
+      'import/prefer-default-export': 0,
+    },
+    plugins: {
+      import: recommended.plugins.import,
+    },
+    extends: [recommended],
+  },
+  source,
+  test,
+]);
